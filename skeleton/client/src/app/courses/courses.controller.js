8c1c98996cf63ce5c6e5dc916c6999d5c2dc558d
Matt Billock <matt.billock@invoice2go.com>
2016-03-21 22:39:21 -0700

    Added CRUD and new template

--- skeleton/client/src/app/courses/courses.controller.js
+++ skeleton/client/src/app/courses/courses.controller.js
@@ -36,9 +36,58 @@
   function CoursesCtrl(CoursesService, coursesList) {
     var vm = this;
     vm.courses = null;
+    vm.loading = [];
 
     //get the courses from the resolve
     vm.courses = coursesList.data.data;
+
+    function readCourses(){
+      CoursesService.list().then(
+        function(coursesList){
+          vm.courses = coursesList.data.data;
+        }
+      )
+    }
+
+    vm.createCourse = function(course, isValid) {
+      if (isValid) {
+        CoursesService.create(course)
+          .then(function (result) {
+            //add the new course locally
+            vm.courses.push(result.data);
+          })
+          .catch(function (reason) {
+            // alert
+            console.log(reason);
+          });
+      }
+    };
+
+    vm.updateCourse = function(course, index){
+      vm.loading[index] = true;
+
+      CoursesService.update(course).
+        then(function (result) {
+          //do something good
+          vm.loading[index] = false;
+        })
+        .catch(function (reason) {
+          // alert
+          console.log(reason);
+        });
+    };
+
+    vm.deleteCourse = function(id){
+      CoursesService.destroy(id).
+        then(function (result) {
+          //refresh the course list
+          readCourses();
+        })
+        .catch(function (reason) {
+          // alert
+          console.log(reason);
+        });
+    };
   }
 
 
