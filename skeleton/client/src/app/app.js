ac1fc6b4d5a9c248f3428d0a57f250648e20096a
Matt Billock <matt.billock@invoice2go.com>
2016-03-21 21:47:47 -0700

    Hooked up to new app

--- skeleton/client/src/app/app.js
+++ skeleton/client/src/app/app.js
@@ -9,9 +9,9 @@
     $urlRouterProvider.otherwise('/');
     $logProvider.debugEnabled(true);
 
-    BackandProvider.setAppName('eduh1');
+    BackandProvider.setAppName('coursemanagerwebinar');
     //BackandProvider.setSignUpToken('0b9bf00e-31f8-4d49-932b-ae0f687bd2d7');
-    BackandProvider.setAnonymousToken('557c10bf-d75a-440e-8281-66712c528be4');
+    BackandProvider.setAnonymousToken('600f494f-6f56-4f42-b8f7-fe9c3ac15982');
 
     //BackandProvider.setAppName('-Your App Name --');
     //BackandProvider.setAnonymousToken('Your Anonymous Token');
